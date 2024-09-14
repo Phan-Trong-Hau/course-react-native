5fa9eb833958b6b112b07aee1e89a45f4227a039
PhanTrongHau <haupk2003@gmail.com>
2024-09-15 00:33:42 +0700

    feat: add prettierrc file

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -2,54 +2,56 @@ import { FontAwesome } from "@expo/vector-icons";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 const styles = StyleSheet.create({
-    footerContainer: {
-      width: "100%",
-      flexDirection: "row",
-      justifyContent: "space-around",
-      alignItems: "center",
-      padding: 20,
-    },
-    footerItem: {
-      alignItems: "center",
-    },
-    footerText: {
-      fontSize: 12,
-      color: "#666",
-      marginTop: 4,
-    },
-  });
+  footerContainer: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    padding: 20,
+  },
+  footerItem: {
+    alignItems: "center",
+  },
+  footerText: {
+    fontSize: 12,
+    color: "#666",
+    marginTop: 4,
+  },
+});
 
-const Footer = () => (
+const Footer = () => {
+
+  return (
     <View style={styles.footerContainer}>
-    <TouchableOpacity
-      style={styles.footerItem}
-      onPress={() => {
-        /* Handle Home press */
-      }}
-    >
-      <FontAwesome name="home" size={24} color="#666" />
-      <Text style={styles.footerText}>Home</Text>
-    </TouchableOpacity>
-    <TouchableOpacity
-      style={styles.footerItem}
-      onPress={() => {
-        /* Handle Explore press */
-      }}
-    >
-      <FontAwesome name="globe" size={24} color="#666" />
-      <Text style={styles.footerText}>Explore</Text>
-    </TouchableOpacity>
-    <TouchableOpacity
-      style={styles.footerItem}
-      onPress={() => {
-        /* Handle Profile press */
-      }}
-    >
-      <FontAwesome name="user" size={24} color="#666" />
-      <Text style={styles.footerText}>Profile</Text>
-    </TouchableOpacity>
-  </View>
+      <TouchableOpacity
+        style={styles.footerItem}
+        onPress={() => {
+          /* Handle Home press */
+        }}
+      >
+        <FontAwesome name="home" size={24} color="#666" />
+        <Text style={styles.footerText}>Home</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.footerItem}
+        onPress={() => {
+          /* Handle Explore press */
+        }}
+      >
+        <FontAwesome name="globe" size={24} color="#666" />
+        <Text style={styles.footerText}>Explore</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.footerItem}
+        onPress={() => {
+          /* Handle Profile press */
+        }}
+      >
+        <FontAwesome name="user" size={24} color="#666" />
+        <Text style={styles.footerText}>Profile</Text>
+      </TouchableOpacity>
+    </View>
   );
-
+}
 
 export default Footer;
